a7fa19d1455d38642a13a6cc280f0566dd56c809
bonekyaw <phonenai2014@gmail.com>
2024-09-16 16:39:06 +0630

    html css jQuery added

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-document.getElementById("image").addEventListener("click", function () {
-    document.getElementById("text").textContent = "You Clicked it!";
-})
+const image = document.getElementById("image"); // <img>
+const paragraph = document.getElementById("text"); // <p></p>
+
+const user = {
+  firstName: "David",
+  lastName: "Beckham",
+  age: 45,
+  favourites: {
+    music: ["Classic", "Rock"],
+    sport: ["football", "MMA"],
+  },
+};
+
+image.onclick = () => {
+  const newUser = {
+    ...user,
+    favourites: {
+      ...user.favourites,
+      music: [...user.favourites.music],
+      sport: [...user.favourites.sport],
+    },
+  }; // new object
+  newUser.favourites.music.push("Hiphop");
+  console.log(user);
+  // paragraph.textContent = newUser;
+};
+
+// Deep Clone
