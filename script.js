becd64e002fb169a3410da7a1064d39b08489abb
bonekyaw <phonenai2014@gmail.com>
2024-10-08 18:06:21 +0630

    jquery

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
-const image = document.getElementById("image"); // <img>
-const paragraph = document.getElementById("text"); // <p></p>
+// const image = document.getElementById("image"); // <img>
+// const paragraph = document.getElementById("text"); // <p></p>
 
-const user = {
-  firstName: "David",
-  lastName: "Beckham",
-  age: 45,
-  favourites: {
-    music: ["Classic", "Rock"],
-    sport: ["football", "MMA"],
-  },
-};
+// image.onclick = () => {
+//   paragraph.textContent = "Change Text";
+// }
 
-image.onclick = () => {
-  const newUser = {
-    ...user,
-    favourites: {
-      ...user.favourites,
-      music: [...user.favourites.music],
-      sport: [...user.favourites.sport],
-    },
-  }; // new object
-  newUser.favourites.music.push("Hiphop");
-  console.log(user);
-  // paragraph.textContent = newUser;
-};
+$(document).ready(function() {
+  $("#image").click(function() {
+    $("#text").text("Hello jQuery");
+  });
+});
+
+// const user = {
+//   firstName: "David",
+//   lastName: "Beckham",
+//   age: 45,
+//   favourites: {
+//     music: ["Classic", "Rock"],
+//     sport: ["football", "MMA"],
+//   },
+// };
+
+// image.onclick = () => {
+//   const newUser = {
+//     ...user,
+//     favourites: {
+//       ...user.favourites,
+//       music: [...user.favourites.music],
+//       sport: [...user.favourites.sport],
+//     },
+//   }; // new object
+//   newUser.favourites.music.push("Hiphop");
+//   console.log(user);
+//   // paragraph.textContent = newUser;
+// };
 
 // Deep Clone
