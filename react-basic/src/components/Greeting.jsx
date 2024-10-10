31b419e00f5c4bfed220c6d9f77107aa08e30c63
bonekyaw <phonenai2014@gmail.com>
2024-10-10 17:45:33 +0630

    react basic

---
+++ react-basic/src/components/Greeting.jsx
@@ -0,0 +1,11 @@
+const Greeting = () => (
+  <>
+    <h1>Hello React</h1>
+    <p>This is paragraph</p>
+  </>
+);
+
+export const color = "green";
+export const size = 28;
+
+export default Greeting;
