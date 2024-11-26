c1e1d79e6eb054d1b25bd5c1c6cdd5bffda4979f
bonekyaw <phonenai2014@gmail.com>
2024-11-26 23:02:03 +0630

    Optimization explained

--- react-basic/src/components/InitialSetup.tsx
+++ react-basic/src/components/InitialSetup.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect, useCallback } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 // Props and types for InitialSetup
 interface InitialSetupProps {
@@ -11,7 +11,7 @@ const InitialSetup = ({ onSetup }: InitialSetupProps) => {
 
   const [inputValue, setInputValue] = useState<string>("");
 
-  const handleSubmit = useCallback(() => {
+  const handleSubmit = () => {
     console.log("handleSubmit function called");
 
     const numericValue = Number(inputValue); // "7" => 7
@@ -20,17 +20,7 @@ const InitialSetup = ({ onSetup }: InitialSetupProps) => {
     } else {
       alert("Please enter a valid number.");
     }
-  }, [onSetup]);
-
-  // Ref to store the current handleSubmit reference
-  // const handleSubmitRef = useRef(handleSubmit);
-
-  // useEffect(() => {
-  //   if (handleSubmitRef.current !== handleSubmit) {
-  //     console.log("handleSubmit function recreated");
-  //     handleSubmitRef.current = handleSubmit;
-  //   }
-  // });
+  };
 
   return (
     <>
