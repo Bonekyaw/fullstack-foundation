43db1ba1aa6f31a35ecdf3db336397e4688372fd
bonekyaw <phonenai2014@gmail.com>
2025-02-26 18:47:16 +0630

    input otp testing

---
+++ shadcn-tail4/src/App.tsx
@@ -0,0 +1,27 @@
+import { REGEXP_ONLY_DIGITS_AND_CHARS } from "input-otp";
+
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+
+function App() {
+  return (
+    <>
+      <p>Click on the Vite and React logos to learn more</p>
+      <InputOTP maxLength={6} pattern={REGEXP_ONLY_DIGITS_AND_CHARS}>
+        <InputOTPGroup>
+          <InputOTPSlot index={0} />
+          <InputOTPSlot index={1} />
+          <InputOTPSlot index={2} />
+          <InputOTPSlot index={3} />
+          <InputOTPSlot index={4} />
+          <InputOTPSlot index={5} />
+        </InputOTPGroup>
+      </InputOTP>
+    </>
+  );
+}
+
+export default App;
